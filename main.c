4fbe4550fe2009637ecce73823145b258aebc780
totweva <totweva@gmail.com>
2022-08-17 22:43:57 +0100

    first test commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * message
+ * return: 0
+ */
+
+int main(void)
+{
+	int i;
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#endif /* MAIN_H */
